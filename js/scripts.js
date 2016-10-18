e51b12298b2a09cc2a2e9b9ac1bc21656dfc522d
Adam Baker and Devin Ludwig <devin@>
2016-10-18 16:32:44 -0700

    allow response to be removed

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,15 @@ $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#cat").prepend("<li>Meow!</li>");
     $("ul#dog").prepend("<li>Woof!</li>");
+    $("ul#cat").children("li").first().click(function() {
+    $(this).remove();
   });
-
+  $("ul#dog").children("li").first().click(function() {
+  $(this).remove();
+});
+});
   $("button#woof").click(function() {
     $("ul#cat").prepend("<li>Meow!</li>");
     $("ul#dog").prepend("<li>Woof!</li>");
   });
-
-  $("ul#cat").children("li").first().click(function() {
-  $(this).remove();
-});
-  $("ul#dog").children("li").first().click(function() {
-  $(this).remove();
-});
 });
