b98d6e29476505a497ee872ae34c9364b9f0dee6
Adam Baker and Devin Ludwig <devin@>
2016-10-18 16:25:05 -0700

    cat vs dog

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("button#meow").click(function() {
+    $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#dog").prepend("<li>Woof!</li>");
+  });
+
+  $("button#woof").click(function() {
+    $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#dog").prepend("<li>Woof!</li>");
+  });
+
+  $("ul#cat").children("li").first().click(function() {
+  $(this).remove();
+});
+  $("ul#dog").children("li").first().click(function() {
+  $(this).remove();
+});
+});
