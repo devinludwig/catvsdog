baa194841e7beb34d6a1d41df91a4650e7c3b7c9
Adam Baker and Devin Ludwig <adam+devin@>
2016-10-18 16:41:38 -0700

    add fixes to javascript file.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,20 @@ $(document).ready(function() {
     $("ul#cat").prepend("<li>Meow!</li>");
     $("ul#dog").prepend("<li>Woof!</li>");
     $("ul#cat").children("li").first().click(function() {
-    $(this).remove();
-  });
-  $("ul#dog").children("li").first().click(function() {
-  $(this).remove();
-});
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
 });
   $("button#woof").click(function() {
     $("ul#cat").prepend("<li>Meow!</li>");
     $("ul#dog").prepend("<li>Woof!</li>");
+    $("ul#cat").children("li").first().click(function() {
+    $(this).remove();
+  });
+  $("ul#dog").children("li").first().click(function() {
+  $(this).remove();
+  });
   });
 });
